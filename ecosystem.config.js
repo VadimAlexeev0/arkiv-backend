39aeefa6efb9e074265084fdf3b22ab847efd8d7
Vadim Alexeev <valexeev30@gmail.com>
2023-06-12 19:34:14 +0100

    pm2 server config

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	apps: [
+		{
+			name: "Arkiv-Listener",
+			script: "npm",
+			args: "run start",
+		},
+	],
+
+	deploy: {
+		production: {
+			user: "vadim",
+			host: "107.189.2.120",
+			ref: "origin/master",
+			repo: "git@github.com:VadimAlexeev0/arkiv-backend.git",
+			path: "/arkiv-listener",
+			"pre-setup": "npm install -g ts-node",
+			"pre-deploy-local": "git fetch origin main",
+			"post-deploy":
+				//"npm install && pm2 reload ecosystem.config.js --env production",
+				"npm install && pm2 restart ecosystem.config.js --env production",
+		},
+	},
+};
